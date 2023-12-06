ecb9f63a35659216a999713d1487995b90dc2559
Zion livingstone <zionlivingstone4@gmail.com>
2023-12-07 00:30:33 +0100

    x

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Nav from './components/nav'
-import Gallery from './components/Gallery'
-import Contact from './components/Contact'
-import Reviews from './components/Reviews'
-import Footer from './components/Footer'
+
 const inter = Inter({ subsets: ['latin'] })
 
 
@@ -13,12 +9,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Nav />
+
         {children}
-        <Gallery />
-        <Reviews />
-        <Contact />
-        <Footer />
+
 
         </body>
     </html>
--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,6 @@
 "use Client"
 
-import Hero from './components/hero'
-import Nav from './components/nav'
-import Features from './components/features'
-import Contact from './components/Contact'
-import Numbers from './components/numbers'
-import Text from './components/Text'
-import Gallery from './components/Gallery'
-import Reviews from './components/Reviews'
-import Footer from './components/Footer'
+
 import { Poppins,Open_Sans } from 'next/font/google'
 
 const poppins = Poppins({ 
@@ -25,10 +17,7 @@ export default function Home() {
   return (
     
     <div>
-      <Hero />
-      <Text />
-      <Features />
-      <Numbers />
+
     </div>
   )
 }
